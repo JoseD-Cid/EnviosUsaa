4727045b2cd24e25eed4fe9b95d6bbe338f68306
Efrain Reyes <joseeaguirree6@gmail.com>
2025-04-27 18:25:27 -0600

    Actualizacion de vista dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                 <div class="collapse navbar-collapse" id="navbarDashboard">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">   
-                            <a class="nav-link active fw-semibold" href="{{ route('dashboard') }}">Menu</a>
+                            <a class="nav-link active fw-semibold" href="{{ route('dashboard') }}">Dashboard</a>
                         </li>
                         @can('crear clientes')
                         <li class="nav-item">
